cb826efaef6508953964e5a0ec078c9fb5e3032b
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-04 12:56:54 +0200

    add third script correction

--- snack-3/script.js
+++ snack-3/script.js
@@ -9,3 +9,16 @@ for (let i = 0; i < 10; i++) {
 
 // 2. Sommo e stampo in console tutti i numeri inseriti
 console.log(userNumber + userNumber + userNumber + userNumber + userNumber + userNumber + userNumber + userNumber + userNumber + userNumber);
+
+
+// Correzione
+// 1. Chiedo per 10 volte un numero
+// let number;
+// let total = 0;
+// for (let i = 0; i < 10; i++) {
+//     number = Number(prompt("Inserisci un numero"));
+//     2. Sommo tutti i numeri inseriti
+//     total += number;
+// }
+// 3. Stampo in console il totale
+// console.log(total);
