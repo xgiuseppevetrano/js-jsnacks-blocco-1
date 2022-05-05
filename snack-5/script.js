f78d8d8493d980899316fe5891e4c7f23542bbcb
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-05 17:47:19 +0200

    add snack-5

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,1 +1,14 @@
 // Inserisci un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+// 1. Chiedo di inserire un numero
+const askNumber = Number(prompt("Inserisci un numero"));
+
+// 2. SE il numero è pari
+//    ALLORA stampo il numero in console
+if (askNumber % 2 === 0) {
+    console.log(askNumber);
+    // 3. Se il numero è dispari
+    //    ALLORA stampo il numero successivo in console
+} else if (askNumber % 2 === 1) {
+    console.log(askNumber + 1);
+}
