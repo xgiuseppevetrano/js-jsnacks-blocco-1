659b8359feeaa1b886b6d0909a420d51683b6dca
xgiuseppevetrano <xgiuseppevetrano@gmail.com>
2022-05-05 18:25:27 +0200

    add snack-7

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,1 +1,18 @@
-// Crea un array di numeri interi e fai la somma di tutti gli elementi  che sono in posizione dispari
+// Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
+
+// 1. Creo un array di numeri interi casuali tra 1 e 100
+const number = [];
+for (let i = 0; i < 20; i++) {
+    number.push(Math.floor(Math.random() * 100) + 1);
+}
+
+// 2. SE gli elementi dentro l'array sono in posizione dispari
+//    ALLORA li sommo fra di loro
+let total = 0;
+for (let i = 0; i < number.length; i++) {
+    if (i % 2 === 1) {
+        total += number[i];
+    }
+}
+
+console.log(total);
